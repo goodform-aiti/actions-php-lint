707d41f588d25c5093ce2f492c28991d479fd757
Amir Alian <amircup2006@gmail.com>
2020-11-18 16:44:41 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,7 +20,8 @@ if [ ! -d "${DIR_TO_SCAN}" ] && [ ! -f "${DIR_TO_SCAN}" ]; then
 fi
 
 
-CHANGED_FILES=$(git diff --name-only --diff-filter=AM master...HEAD)
+CHANGED_FILES=$(git diff --name-only --diff-filter=AM ${{ github.event.before }} ${{ github.sha }})
+#CHANGED_FILES=$(git diff --name-only --diff-filter=AM master...HEAD)
 #CHANGED_FILES_PHP=$(echo "$CHANGED_FILES" | grep -P "(\.phtml|\.php)$" | grep -v -P "^((?:lib/phpseclib/)|(?:lib/Zend)|(?:/lib/PEAR)|(?:.phpstorm.meta.php)).+")
 
 
