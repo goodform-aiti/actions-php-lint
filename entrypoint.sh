39bc188d08bd83f5e178e978405e5c70142cde3c
Amir Alian <amircup2006@gmail.com>
2020-11-19 10:01:36 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,7 +24,7 @@ echo $CURRENT_COMMIT
 echo "###PREVIOUS COMMIT"
 echo $PREVIOUS_COMMIT
 #CHANGED_FILES=$(git diff --name-only --diff-filter=AM ${PREVIOUS_COMMIT} ${CURRENT_COMMIT})
-echo $(${CHANGED_FILES})
+git diff --name-only --diff-filter=AM 4a36d5c123f95a6d0efe9e79ed1839061fff2a7b 1a8430d6d057a05d050778b2c1076a2d632ced50
 #CHANGED_FILES=$(git diff --name-only --diff-filter=AM master...HEAD)
 #CHANGED_FILES_PHP=$(echo "$CHANGED_FILES" | grep -P "(\.phtml|\.php)$" | grep -v -P "^((?:lib/phpseclib/)|(?:lib/Zend)|(?:/lib/PEAR)|(?:.phpstorm.meta.php)).+")
 
