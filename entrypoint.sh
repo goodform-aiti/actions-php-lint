65ea1c55c7d0326561e2e2eac63c4d34a9326fab
Amir Alian <amircup2006@gmail.com>
2020-11-19 11:28:42 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,45 +1,13 @@
 #!/bin/sh
 set -e
 
-PHP_FULL_VERSION=$(php -r 'echo phpversion();')
 
-if [ -z "$1" ]; then
-  DIR_TO_SCAN="."
-else
-  DIR_TO_SCAN="$1"
-fi
 
-echo "## Running PHP Syntax Checker (lint) on ${DIR_TO_SCAN}"
-echo "PHP Version : ${PHP_FULL_VERSION}"
-
-if [ ! -d "${DIR_TO_SCAN}" ] && [ ! -f "${DIR_TO_SCAN}" ]; then
-  echo "\nInvalid directory or file: ${DIR_TO_SCAN}"
-  echo "\n\n"
-
-  exit 2
-fi
-
-echo "###LATEST COMMIT:"
-echo $CURRENT_COMMIT
-echo "###PREVIOUS COMMIT"
-echo $PREVIOUS_COMMIT
 #CHANGED_FILES=$(git diff --name-only --diff-filter=AM ${PREVIOUS_COMMIT} ${CURRENT_COMMIT})
-pwd
-ls -la
+#pwd
+#ls -la
 #CHANGED_FILES=$(git diff --name-only --diff-filter=AM master...HEAD)
 #CHANGED_FILES_PHP=$(echo "$CHANGED_FILES" | grep -P "(\.phtml|\.php)$" | grep -v -P "^((?:lib/phpseclib/)|(?:lib/Zend)|(?:/lib/PEAR)|(?:.phpstorm.meta.php)).+")
 
 
-
-
-ERROR=0
-for file in $(find ${DIR_TO_SCAN} -type f -name "*.php" ! -path "./vendor/*"); do
-  RESULTS=$(php -l ${file} || true)
-
-  if [ "${RESULTS}" != "No syntax errors detected in ${file}" ]; then
-    echo "\n${RESULTS}\n"
-    ERROR=1
-  fi
-done
-
-exit "${ERROR}"
+git fetch --all
