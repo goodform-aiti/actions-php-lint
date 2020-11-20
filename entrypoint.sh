4749b630dead60d217928c00b75bad3cb268d78c
Amir Alian <amircup2006@gmail.com>
2020-11-20 15:56:35 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,38 +2,12 @@
 set -e
 
 CHANGED_FILES=$(git diff --name-only --diff-filter=AM ${PREVIOUS_COMMIT} ${CURRENT_COMMIT})
-CHANGED_FILES_PHP=$(find ${CHANGED_FILES} -type f -regex "^.*\(\.php\|\.phtml\)$" ! -path "./vendor/*")
-
+CHANGED_FILES_PHP=$(find ${CHANGED_FILES} -type f -regex "^.*\(\.php\|\.phtml\)$")
+CHANGED_CORE_FILES=$(find ${CHANGED_FILES} -type f -regex "^\(app/code/core\|app/design/frontend/base\|app/design/adminhtml/base\)/.+$")
 
 PHP_FULL_VERSION=$(php -r 'echo phpversion();')	
-
-
-if [ -z "$1" ]; then	
-  DIR_TO_SCAN="."	
-else	
-  DIR_TO_SCAN="$1"	
-fi	
-
-
 echo "## Running PHP Syntax Checker (lint) on ${DIR_TO_SCAN}"	
 echo "PHP Version : ${PHP_FULL_VERSION}"	
-
-if [ ! -d "${DIR_TO_SCAN}" ] && [ ! -f "${DIR_TO_SCAN}" ]; then	
-  echo "\nInvalid directory or file: ${DIR_TO_SCAN}"	
-  echo "\n\n"	
-
-  exit 2	
-fi	
-
-echo "ls:"
-ls
-
-
-#CHANGED_FILES=$(git diff --name-only --diff-filter=AM master...HEAD)	#CHANGED_FILES=$(git diff --name-only --diff-filter=AM master...HEAD)
-#CHANGED_FILES_PHP=$(cat CHANGED_FILES | grep -P "(\.phtml|\.php)$" | grep -v -P "^((?:lib/phpseclib/)|(?:lib/Zend)|(?:/lib/PEAR)|(?:.phpstorm.meta.php)).+")	#CHANGED_FILES_PHP=$(echo "$CHANGED_FILES" | grep -P "(\.phtml|\.php)$" | grep -v -P "^((?:lib/phpseclib/)|(?:lib/Zend)|(?:/lib/PEAR)|(?:.phpstorm.meta.php)).+")
-
-# CHANGED_FILES_PHP:
-#echo ${CHANGED_FILES_PHP}
 
 
 ERROR=0	
@@ -48,21 +22,12 @@ done
 
 
 
-# echo "CHANGED_FILES_PHP start:"
-# echo ${CHANGED_FILES_PHP}
-# echo "CHANGED_FILES_PHP end"
 
-
-# echo "CHANGED_CORE_FILES start:"
-# echo ${CHANGED_CORE_FILES}
-# echo "CHANGED_CORE_FILES end"
-
-
-# for file in ${CHANGED_CORE_FILES}; do		
-#     RESULT="Core file is changed: ${file}"
-#     echo "\n${RESULTS}\n"	
-#     ERROR=1	
-# done	
+for file in ${CHANGED_CORE_FILES}; do		
+    RESULT="Core file is changed: ${file}"
+    echo "\n${RESULTS}\n"	
+    ERROR=1	
+done	
 
 
 
