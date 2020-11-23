c793bf3ecf58476a6ce3557a4e64e9ce1671b952
Amir Alian <amircup2006@gmail.com>
2020-11-23 15:35:19 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,7 +19,7 @@ echo "*******************git log:"
 git log
 
 
-CHANGED_FILES=$(git diff --name-only --diff-filter=AM main)
+CHANGED_FILES=$(git diff --name-only --diff-filter=AM main...my-test)
 CHANGED_FILES_PHP=$(find ${CHANGED_FILES} -type f -regex "^.*\(\.php\|\.phtml\)$")
 
 PHP_FULL_VERSION=$(php -r 'echo phpversion();')	
