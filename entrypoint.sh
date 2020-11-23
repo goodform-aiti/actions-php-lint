8bd65f1310db63e1241333b4b6a8501bea9430d9
Amir Alian <amircup2006@gmail.com>
2020-11-23 16:37:06 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 git fetch origin main
 git checkout main
-CHANGED_FILES=$(git diff --name-only --diff-filter=AM ${CURRENT_COMMIT}...main)
+CHANGED_FILES=$(git diff --name-only --diff-filter=AM main...test2)
 CHANGED_FILES_PHP=$(find ${CHANGED_FILES} -type f -regex "^.*\(\.php\|\.phtml\)$")
 
 echo " ************** changed files"
