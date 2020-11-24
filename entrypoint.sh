d6091b9be214acc8f5b35f51832fdb5d7d97b1b3
Amir Alian <amircup2006@gmail.com>
2020-11-24 12:22:55 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,15 @@ set -e
 
 echo "*******************"
 echo ${MODIFIED_FILES}
+echo "*******************"
+array=(${string//:/ })
+for i in "${!MODIFIED_FILES[@]}"
+do
+    echo "$i=>${array[i]}"
+done
+echo "*******************"
+
+
 
 exit 0
 
