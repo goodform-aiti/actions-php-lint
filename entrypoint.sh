f176a29e22044c6d57bb8576f9eeddd44c60a151
Amir Alian <amircup2006@gmail.com>
2020-11-25 11:36:43 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 set -e
 
-CHANGED_FILES_PHP=$(find ${MODIFIED_FILES} -type f -regex "^.*\(\.php\|\.phtml\)$")
 
 echo " ************** MODIFIED FILES"
 echo ${MODIFIED_FILES}
-
-echo "**************** changed files php"
-echo ${CHANGED_FILES_PHP}
+echo " ******************************"
 
 PHP_FULL_VERSION=$(php -r 'echo phpversion();')	
 echo "## Running PHP Syntax Checker (lint) on ${DIR_TO_SCAN}"	
@@ -15,11 +12,15 @@ echo "PHP Version : ${PHP_FULL_VERSION}"
 
 
 ERROR=0	
-for file in ${CHANGED_FILES_PHP}; do	
-  if ! php -d error_reporting="E_ALL & ~E_DEPRECATED" -l "$file"; then
-    ERROR=1
-  fi
-done	
+paths=(${MODIFIED_FILES//,/ })
+for i in "${!paths[@]}"
+do
+    if [[ ${paths[i]} =~ ^(.+)\.php|phtml$ ]] ; then
+       if ! php -d error_reporting="E_ALL & ~E_DEPRECATED" -l "${paths[i]}"; then
+          ERROR=1
+       fi
+    fi
+done
 
 
 exit "${ERROR}"
