72b8c7367f13029602e519dd4b376b2f6d186c27
Luboš Hubáček <seki.php@gmail.com>
2024-02-13 21:12:32 +0100

    Update entrypoint.sh - fix error with newline

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 set -e
 
-
-echo " ************** MODIFIED FILES"
+echo "************** MODIFIED FILES **************"
 
 printf ${MODIFIED_FILES}
 
-echo " ******************************"
+echo "********************************************"
 
-PHP_FULL_VERSION=$(php -r 'echo phpversion();')	
-echo "## Running PHP Syntax Checker (lint) on ${DIR_TO_SCAN}"	
-echo "PHP Version : ${PHP_FULL_VERSION}"	
+PHP_FULL_VERSION=$(php -r 'echo phpversion();')
+echo "## Running PHP Syntax Checker (lint) on ${DIR_TO_SCAN}"
+echo "PHP Version : ${PHP_FULL_VERSION}"
 
 
-ERROR=0	
-paths=(${MODIFIED_FILES//\n / })
-for i in "${!paths[@]}"
+ERROR=0
+paths=(${MODIFIED_FILES//\\n/ })
+for i in ${!paths[@]}
 do
     if [[ ! ${paths[i]} =~ ^(.+)\.(php|phtml)$ ]] ; then
         echo "skip: ${paths[i]} (this is not php)"
@@ -30,9 +29,12 @@ do
       continue
    fi
 
-   if ! php -d error_reporting="E_ALL & ~E_DEPRECATED" -l "${paths[i]}" # checking php syntax
+   # checking PHP syntax
+   if ! php -d error_reporting="E_ALL & ~E_DEPRECATED" -l "${paths[i]}"
    then
       ERROR=1
+   else
+      echo "[OK] ${paths[i]}"
    fi
 done
 
