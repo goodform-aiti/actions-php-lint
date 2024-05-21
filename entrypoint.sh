1a2905acc57dc4b5fe1cb19d0dc25ace637c258e
Petr Tobiáš <petr.tobias@ateli.cz>
2024-05-21 16:13:54 +0200

    GF2-265 bump to PHP 8.2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 
+echo "************** MODIFIED FILES **************"
+echo "${MODIFIED_FILES}"
+echo "********************************************"
+
 PHP_FULL_VERSION=$(php -r 'echo phpversion();')
 
 if [ -z "$1" ]; then
@@ -19,14 +23,7 @@ if [ ! -d "${DIR_TO_SCAN}" ] && [ ! -f "${DIR_TO_SCAN}" ]; then
   exit 2
 fi
 
-ERROR=0
-for file in $(find ${DIR_TO_SCAN} -type f -name "*.php" ! -path "./vendor/*"); do
-  RESULTS=$(php -l ${file} || true)
+find "${DIR_TO_SCAN}" -type f -name '*.php' ! -path './vendor/*' -print0 \
+     | xargs -0 -n 1 php -l | (! grep -Ev '^No syntax errors detected in ')
 
-  if [ "${RESULTS}" != "No syntax errors detected in ${file}" ]; then
-    echo "\n${RESULTS}\n"
-    ERROR=1
-  fi
-done
-
-exit "${ERROR}"
+exit "${?}"
